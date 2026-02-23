cdcf5a2dd545b5c34557ff75b8550e543e4047ca
ajithsrdev <ajithsr2004@gmail.com>
2026-02-23 23:02:37 +0530

    Update sip.js

--- js/sip.js
+++ js/sip.js
@@ -1,1 +1,37 @@
+let sipChart;
 
+function calculateSIP() {
+
+const P = parseFloat(document.getElementById("monthlyInvestment").value);
+const annualRate = parseFloat(document.getElementById("annualReturn").value);
+const years = parseFloat(document.getElementById("years").value);
+
+const r = annualRate / 12 / 100;
+const n = years * 12;
+
+const FV = P * (((Math.pow(1 + r, n) - 1) / r) * (1 + r));
+
+const invested = P * n;
+const returns = FV - invested;
+
+document.getElementById("totalValue").innerText = FV.toFixed(0);
+document.getElementById("totalInvested").innerText = invested.toFixed(0);
+document.getElementById("estimatedReturns").innerText = returns.toFixed(0);
+
+const ctx = document.getElementById("sipChart");
+
+if (sipChart) sipChart.destroy();
+
+sipChart = new Chart(ctx, {
+type: "doughnut",
+data: {
+labels: ["Invested Amount", "Estimated Returns"],
+datasets: [{
+data: [invested, returns],
+backgroundColor: ["#1e3a8a", "#cbd5e1"]
+}]
+}
+});
+}
+
+calculateSIP();
